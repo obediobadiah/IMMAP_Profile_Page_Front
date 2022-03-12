4e80b35f4b7c1f0564a103fe9bfbfcc2b9edcb87
obediobadiah <obediobadiah05@gmail.com>
2022-03-12 06:28:34 +0200

    AddProfile page forms conception

--- src/Components/AddProfile.js
+++ src/Components/AddProfile.js
@@ -14,91 +14,103 @@ import { Link } from 'react-router-dom'
 
 class AddProfile extends Component {
 
-    // constructor(props) {
-    //     super(props);
-
-    //     this.state = {
-    //       fields: {},
-    //       errors: {},
-    //     };
-    // }
-
-    // handleValidation() {
-    //     let fields = this.state.fields;
-    //     let errors = {};
-    //     let formIsValid = true;
-
-    //     // Name
-    //     if (!fields["name"]) {
-    //         formIsValid = false;
-    //         errors["name"] = "Cannot be empty";
-    //     }
-
-    //     if (typeof fields["name"] !== "undefined") {
-    //         if (!fields["name"].match(/^[a-zA-Z]+$/)) {
-    //             formIsValid = false;
-    //             errors["name"] = "Only letters";
-    //         }
-    //     }
-
-    //     //Email
-    //     if (!fields["email"]) {
-    //         formIsValid = false;
-    //         errors["email"] = "Cannot be empty";
-    //     }
-
-    //     if (typeof fields["email"] !== "undefined") {
-    //         let lastAtPos = fields["email"].lastIndexOf("@");
-    //         let lastDotPos = fields["email"].lastIndexOf(".");
-
-    //         if (
-    //             !(
-    //                 lastAtPos < lastDotPos &&
-    //                 lastAtPos > 0 &&
-    //                 fields["email"].indexOf("@@") == -1 &&
-    //                 lastDotPos > 2 &&
-    //                 fields["email"].length - lastDotPos > 2
-    //             )
-    //         ) {
-    //             formIsValid = false;
-    //             errors["email"] = "Email is not valid";
-    //         }
-    //     }
-
-    //     this.setState({ errors: errors });
-    //     return formIsValid;
-    // }
-
-    // contactSubmit(e) {
-    //     e.preventDefault();
     
-    //     if (this.handleValidation()) {
-    //       alert("Form submitted");
-    //     } else {
-    //       alert("Form has errors.");
-    //     }
-    //   }
-    
-    //   handleChange(field, e) {
-    //     let fields = this.state.fields;
-    //     fields[field] = e.target.value;
-    //     this.setState({ fields });
-    //   }
+
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            file: null
+        }
+        this.state = { checked: false };
+        this.handleChange = this.handleChange.bind(this);
+        this.uploadSingleFile = this.uploadSingleFile.bind(this)
+    }
+
+    handleChange() {
+        this.setState({
+          checked: !this.state.checked
+      })
+    }
+
+    uploadSingleFile(e) {
+        this.setState({
+            file: URL.createObjectURL(e.target.files[0])
+        })
+    }
 
     render() {
+
+        let imgPreview;
+        if (this.state.file) {
+            imgPreview = <img src={this.state.file} alt='' />;
+        }
+
+        const content = !this.state.checked 
+    	? <div className='col-md-6 col-sm-12 exp_date' id='end_dates'>
+
+        <select class="profile_date_month form-select" aria-label="Default select example" required>
+            <option selected disabled hidden>End Month</option>
+            <option value="January">January</option>
+            <option value="February">February</option>
+            <option value="March">March</option>
+            <option value="April">April</option>
+            <option value="May">May</option>
+            <option value="June">June</option>
+            <option value="July">July</option>
+            <option value="August">August</option>
+            <option value="September">September</option>
+            <option value="October">October</option>
+            <option value="November">November</option>
+            <option value="December">December</option>
+        </select>
+
+        <select class="profile_date_year form-select" aria-label="Default select example" required>
+            <option selected disabled hidden>End Year</option>
+            <option value="2000">2000</option>
+            <option value="2001">2001</option>
+            <option value="2002">2002</option>
+            <option value="2003">2003</option>
+            <option value="2004">2004</option>
+            <option value="2005">2005</option>
+            <option value="2006">2006</option>
+            <option value="2007">2007</option>
+            <option value="2008">2008</option>
+            <option value="2009">2009</option>
+            <option value="2010">2010</option>
+            <option value="2011">2011</option>
+            <option value="2012">2012</option>
+            <option value="2013">2013</option>
+            <option value="2014">2014</option>
+            <option value="2015">2015</option>
+            <option value="2016">2016</option>
+            <option value="2017">2017</option>
+            <option value="2018">2018</option>
+            <option value="2019">2019</option>
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+        </select>
+    </div>
+      : null;
 
         return (
             <div className='container'>
                 <div className='row'>
                     <form action="" method="get" >
                         <div className='identity'>
-                            <div className="profile_picture">
+                            <div className="mb-3 profile_picture">
+                                <div>
+                                    <img src={this.state.file} alt='' className="profile_picture_img" />
+                                </div>
+                                <input type="file" className="mb-3 form-control" onChange={this.uploadSingleFile} />
                             </div>
                             <div class="mb-3">
-                                <input type="text" class="profile_name form-control " placeholder="Full Name" />
+
+                                <input type="text" class="profile_name form-control " placeholder="Full Name" required/>
                             </div>
                             <select class="profile_age form-select" aria-label="Default select example">
-                                <option selected>Select Age</option>
+                                <option selected disabled hidden>Select Age</option>
                                 <option value="1">1</option>
                                 <option value="2">2</option>
                                 <option value="3">3</option>
@@ -150,18 +162,18 @@ class AddProfile extends Component {
                             <div className="work_experience">
                                 <h1>Work Experiences</h1>
                             </div>
-                            <input type="text" class="job_title form-control" placeholder="Job Title" />
+                            <input type="text" class="job_title form-control" placeholder="Job Title" required/>
                             <div class="job_checkbox form-check">
-                                <input class="job_check form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                <input class="job_check form-check-input" type="checkbox" value="" id="flexCheckDefault" checked={ this.state.checked } onChange={ this.handleChange }/>
                                 <label class="checkbox form-check-label" for="flexCheckDefault">
                                     Current Position
                                 </label>
                             </div>
 
                             <div className='experience_dates'>
-                                <div className='col-md-6 exp_date' id='end_dates'>
+                                <div className='col-md-6 col-sm-12 exp_date' id='start_dates'>
                                     <select class="profile_date_month form-select" aria-label="Default select example">
-                                        <option selected>Start Month</option>
+                                        <option selected disabled hidden>Start Month</option>
                                         <option value="January">January</option>
                                         <option value="February">February</option>
                                         <option value="March">March</option>
@@ -176,7 +188,7 @@ class AddProfile extends Component {
                                         <option value="December">December</option>
                                     </select>
                                     <select class="profile_date_year form-select" aria-label="Default select example">
-                                        <option selected>End Year</option>
+                                        <option selected disabled hidden>Start Year</option>
                                         <option value="2000">2000</option>
                                         <option value="2001">2001</option>
                                         <option value="2002">2002</option>
@@ -203,53 +215,7 @@ class AddProfile extends Component {
                                     </select>
 
                                 </div>
-                                <div className='col-md-6 exp_date' id='end_dates'>
-
-                                    <select class="profile_date_month form-select" aria-label="Default select example" required>
-                                        <option selected>End Month</option>
-                                        <option value="January">January</option>
-                                        <option value="February">February</option>
-                                        <option value="March">March</option>
-                                        <option value="April">April</option>
-                                        <option value="May">May</option>
-                                        <option value="June">June</option>
-                                        <option value="July">July</option>
-                                        <option value="August">August</option>
-                                        <option value="September">September</option>
-                                        <option value="October">October</option>
-                                        <option value="November">November</option>
-                                        <option value="December">December</option>
-                                    </select>
-
-                                    <select class="profile_date_year form-select" aria-label="Default select example" required>
-                                        <option selected>Start Year</option>
-                                        <option value="2000">2000</option>
-                                        <option value="2001">2001</option>
-                                        <option value="2002">2002</option>
-                                        <option value="2003">2003</option>
-                                        <option value="2004">2004</option>
-                                        <option value="2005">2005</option>
-                                        <option value="2006">2006</option>
-                                        <option value="2007">2007</option>
-                                        <option value="2008">2008</option>
-                                        <option value="2009">2009</option>
-                                        <option value="2010">2010</option>
-                                        <option value="2011">2011</option>
-                                        <option value="2012">2012</option>
-                                        <option value="2013">2013</option>
-                                        <option value="2014">2014</option>
-                                        <option value="2015">2015</option>
-                                        <option value="2016">2016</option>
-                                        <option value="2017">2017</option>
-                                        <option value="2018">2018</option>
-                                        <option value="2019">2019</option>
-                                        <option value="2020">2020</option>
-                                        <option value="2021">2021</option>
-                                        <option value="2022">2022</option>
-                                    </select>
-
-
-                                </div>
+                                { content }
                             </div>
                             <div className='company_detail'>
                                 <input refs="email" type="mail" class=" company_name form-control " placeholder="Company name" required />
@@ -257,7 +223,7 @@ class AddProfile extends Component {
                             </div>
                             <div className='job_descript'>
                                 <textarea type="text" class="job_description form-control " placeholder="Job description" rows="5" required />
-                                <button class="add_exp">ADD</button>
+                                <button class="add_exp">Add Experiences</button>
                             </div>
 
                             <div className='submit_button'>
